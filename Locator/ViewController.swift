593d34c1f225eeca3e3373a03da630bab7f70d90
Vince O'Sullivan <vjosullivan@gmail.com>
2016-11-08 06:59:08 +0000

    Initial Commit

---
+++ Locator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Locator
+//
+//  Created by Vincent O'Sullivan on 08/11/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
