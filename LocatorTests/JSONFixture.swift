1df4c7512fd49dffb31ec286697470b8aa02f545
Vince O'Sullivan <vjosullivan@gmail.com>
2017-01-19 08:20:12 +0000

    JSONFixture added.

---
+++ LocatorTests/JSONFixture.swift
@@ -0,0 +1,13 @@
+//
+//  JSONFixture.swift
+//  Locator
+//
+//  Created by Vincent O'Sullivan on 19/01/2017.
+//  Copyright © 2017 Vincent O'Sullivan. All rights reserved.
+//
+
+import Foundation
+
+class JSONFixture {
+    
+}
